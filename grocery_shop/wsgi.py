a9e0172dac8cdac30cc12089852ea007cdcd8245
MGShah <mahekgshah2402@gmail.com>
2025-12-11 11:08:14 +0530

    Add WSGI configuration

---
+++ grocery_shop/wsgi.py
@@ -0,0 +1,11 @@
+"""
+WSGI config for grocery_shop project.
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'grocery_shop.settings')
+
+application = get_wsgi_application()
