b5339a16093d2e474967692a966094b64341cd17
MGShah <mahekgshah2402@gmail.com>
2025-12-11 11:08:13 +0530

    Add ASGI configuration

---
+++ grocery_shop/asgi.py
@@ -0,0 +1,11 @@
+"""
+ASGI config for grocery_shop project.
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'grocery_shop.settings')
+
+application = get_asgi_application()
